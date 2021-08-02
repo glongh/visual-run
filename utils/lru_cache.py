b32de482ae70ffa41e49901946142b7e8564a6a2
glongh <glongh@gmail.com>
2021-08-02 02:06:43 -0400

    feat(*): Add class diagram and sequences

--- utils/lru_cache.py
+++ utils/lru_cache.py
@@ -17,7 +17,7 @@ class CacheLRU():
         self.size = size
         self.cache = OrderedDict()
 
-    def get(self, key) -> str or False:
+    def get(self, key: str) -> str or False:
         if key not in self.cache:
             return False
         else:
@@ -25,20 +25,20 @@ class CacheLRU():
             self.cache.move_to_end(key)
             return self.cache[key]
 
-    def set(self, key, item):
+    def set(self, key: str, item: str) -> None:
         # Oa (Parts): cache consists of parts of a solution
-        # Cache eviction manager builds up.
+        # Cache eviction manager starts to builds up.
         self.cache[key] = item
 
         # [Ob (Greed used): All changes to 'cache' have been additive, and optimizing based on information available at
         # the time, in this case what it is available in the 'cache' at the time of the request.
         # Greedy step: Evict an item when needed. Evict the element which is Least Recently Used.
 
-        # A: Moves the key to the end to demonstrate if was recently used
+        # 1: Moves the key to the end to demonstrate if was recently used
         self.cache.move_to_end(key)
-        # B: If the size has been exceeded, we remove the first key, the "Least Recently Used"
+        # 2: If the size has been exceeded, we remove the first key, the "Least Recently Used"
         if len(self.cache) > self.size:
             self.cache.popitem(last=False)
 
-        # Oc (Complete): 'cache' is complete
+        # Oc (Complete): 'cache' is completely build.
 
